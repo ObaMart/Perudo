4ff34bfda9d23c3d9ce5e0204f812cf011fd59a7
ObaMart <75756983+ObaMart@users.noreply.github.com>
2023-04-24 17:12:10 +0000

    Home and lobby

---
+++ client/client.js
@@ -0,0 +1,135 @@
+const socket = io();
+socket.emit("cs-test", "Connection received from client!")
+socket.on("sc-test", (message) => {
+    console.log(message)
+});
+
+const urlParams = new URLSearchParams(window.location.search);
+const page = window.location.pathname
+
+function send() {
+    const message = document.getElementById("inp-test").value;
+    socket.emit("cs-log", message);
+}
+
+//#region home functions
+function home_JoinWithCode() {
+	const code = document.getElementById("join-code-input").value.toUpperCase();
+	const playerName = document.getElementById("player-name-input").value;
+	if (playerName == "") {
+		alert("Je moet een naam invullen.");
+	} else {
+		window.location = `/lobby.html?code=${code}&b=${brightnessSliderValue}&playername=${playerName}`;
+	}
+}
+
+function home_CreateLobby() {
+	const playerName = document.getElementById("player-name-input").value;
+	if (playerName == "") {
+		alert("Je moet een naam invullen.");
+	} else {
+		window.location = `/lobby.html?code=newlobby&b=${brightnessSliderValue}&playername=${playerName}`;
+	}
+}
+
+function home() { // return to home
+	if (confirm("Weet je zeker dat je terug naar het homescherm wil?")) {
+		window.location = "/"
+	}
+}
+//#endregion home functions
+
+//#region brightness
+var brightnessSliderValue = 0.9
+const brightnessSlider = document.getElementById("brightness-slider");
+const urlParamsBrightness = urlParams.get("b")
+if (urlParamsBrightness) {
+	brightnessSliderValue = parseFloat(urlParamsBrightness)
+}
+document.getElementById("dynamic-brightness-overlay").style.backgroundColor = `hsla(0, 0%, 0%, ${0.7 * (1 - brightnessSliderValue)})`
+brightnessSlider.value = brightnessSliderValue;
+var brightnessSliderVisible = false;
+function toggleBrightnessSlider() {
+    if (brightnessSliderVisible) {
+        brightnessSlider.style.opacity = 0;
+        brightnessSliderVisible = false;
+    } else {
+        brightnessSlider.style.opacity = 1;
+        brightnessSliderVisible = true;
+    }
+}
+
+brightnessSlider.oninput = function() {
+	brightnessSliderValue = brightnessSlider.value
+    document.getElementById("dynamic-brightness-overlay").style.backgroundColor = `hsla(0, 0%, 0%, ${0.7 * (1 - brightnessSliderValue)})`
+}
+//#endregion brightness
+
+//#region lobby
+let selected = false;
+let lobbyCode = ""
+if (page == "/lobby.html") {
+	lobbyCode = urlParams.get("code")
+	const clientName = urlParams.get("playername")
+	if (lobbyCode == "newlobby") {
+		alphanum = Array.from("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+		lobbyCode = randomChoice(alphanum) + randomChoice(alphanum) + randomChoice(alphanum) + randomChoice(alphanum);
+		socket.emit("cs-lobby-create", {lobbyCode: lobbyCode, playerName: clientName});
+	} else {
+		document.getElementById("start-game-button").style.display = "none";
+		socket.emit("cs-lobby-join", {lobbyCode: lobbyCode, playerName: clientName});
+	}
+	document.getElementById("lobby-code").innerHTML = lobbyCode
+}
+socket.on("sc-lobby-join-error", data => {
+	const {playerName, errorMessage} = data;
+	if (errorMessage) {
+		alert(errorMessage);
+	} else {
+		// default error message
+		alert("Er is een probleem opgetreden. Je wordt teruggestuurd naar de homepagina.");
+	}
+	window.location = "/"
+});
+socket.on("sc-lobby-player-update", data => {
+	// console.log(data)
+	console.log(Object.entries(data));
+	const clientName = urlParams.get("playername");
+	Object.entries(data).forEach(([colorNum, playerName]) => {
+		if (playerName) document.getElementById(`player${colorNum}-name`).innerHTML = playerName;
+		if (playerName == clientName) selected = true;
+	});
+});
+
+function lobby_ChooseColor(colorNum) {
+	if (selected) {
+		alert("Je hebt al een kleur gekozen.")
+		return
+	}
+	const playerName = urlParams.get("playername");
+	socket.emit("cs-lobby-color-choice", {playerName: playerName, color: colorNum, lobbyCode: lobbyCode});
+	// console.log({playerName: playerName, color: colorNum, lobbyCode: lobbyCode})
+}
+
+function lobby_StartGame() {
+	if (confirm("Wil je het spel starten?")) {
+		const clientName = urlParams.get("playername");
+		socket.emit("cs-lobby-start", {playerName: clientName, lobbyCode: lobbyCode});
+	}
+}
+//#endregion lobby
+
+//#region in-game
+socket.on("sc-game-start", data => {
+	const {lobbyCode} = data;
+	const clientName = urlParams.get("playername");
+	window.location = `/game.html?code=${lobbyCode}&b=${brightnessSliderValue}&playername=${clientName}`;
+});
+// Test if clients are still in socket.io room after game starts (switch html pages)
+//#endregion in-game
+
+//#region general functions
+function randomChoice(arr) {
+	return arr[Math.floor(Math.random()*arr.length)];
+}
+//#endregion general functions
---
+++ index.js
@@ -0,0 +1,62 @@
+"use strict"
+const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const app = express();
+const server = http.Server(app);
+const io = require('socket.io')(server);
+// const server = http.Server(app);
+const PORT = 3000;
+let games = {}
+
+fs.readFile("client/index.html", function(err, html) {
+	if (err) throw err;
+
+	app.use("/", express.static(__dirname + "/client"))
+
+	io.on("connection", (socket) => {
+		console.log(`Connected to socket [${socket.id}]`)
+		socket.on("cs-test", message => {
+			console.log(message);
+		});
+		socket.on("cs-log", message => {
+			console.log(message);
+			socket.broadcast.emit("sc-test", message)
+		});
+		socket.emit("sc-test", "Connection received from socket!")
+
+		//#region lobby
+		socket.on("cs-lobby-create", data => {
+			const {lobbyCode, playerName} = data;
+			games[lobbyCode] = {seats: {1: null, 2: null, 3: null, 4: null, 5: null, 6: null}, joinedPlayers: [playerName], lobbyOwner: playerName}
+			socket.join(lobbyCode)
+		});
+		socket.on("cs-lobby-color-choice", data => {
+			const {playerName, color, lobbyCode} = data;
+			games[lobbyCode].seats[color] = playerName;
+			console.log(games[lobbyCode].seats);
+			io.sockets.in(lobbyCode).emit("sc-lobby-player-update", games[lobbyCode].seats);
+		});
+		socket.on("cs-lobby-join", data => {
+			const {lobbyCode, playerName} = data;
+			console.log(games[lobbyCode].joinedPlayers)
+			if (games[lobbyCode].joinedPlayers.includes(playerName)) {
+				socket.emit("sc-lobby-join-error", {playerName: playerName, errorMessage: `Deze naam (${playerName}) is al gekozen. Kies een unieke naam en join opnieuw.`});
+			} else {
+				games[lobbyCode].joinedPlayers.push(playerName);
+				socket.join(lobbyCode);
+				io.sockets.in(lobbyCode).emit("sc-lobby-player-update", games[lobbyCode].seats);
+			}
+		});
+		socket.on("cs-lobby-start", data => {
+			const {playerName, lobbyCode} = data;
+			if (playerName == games[lobbyCode].lobbyOwner) {
+				io.sockets.in(lobbyCode).emit("sc-game-start", {lobbyCode: lobbyCode});
+			}
+		});
+		//#endregion lobby
+	});
+
+	server.listen(PORT);
+	console.log('Server started.');
+});
